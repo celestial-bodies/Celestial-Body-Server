f831d517c68b2713646ed58d9e2693b0017e8e1b
Ethan Lyons <ethan.lyons84@gmail.com>
2019-03-20 17:01:08 -0600

    Initial commit

---
+++ src/main/java/edu/cnm/deepdive/celestialbodies/celestialbodies/CelestialbodiesApplication.java
@@ -0,0 +1,13 @@
+package edu.cnm.deepdive.celestialbodies.celestialbodies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CelestialbodiesApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CelestialbodiesApplication.class, args);
+  }
+
+}

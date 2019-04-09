8817b918977a9646cbea719946c8d3e4b5208bdd
Todd A Nordquist <toddnordquist144@gmail.com>
2019-04-09 10:35:06 -0600

    Removed OperatorQueryRepository.

--- src/main/java/edu/cnm/deepdive/celestialbodies/controller/StarController.java
+++ src/main/java/edu/cnm/deepdive/celestialbodies/controller/StarController.java
@@ -8,6 +8,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+/**
+ * Defines REST endpoints for servicing requests on {@link Star} resources, invoking {@link
+ * StarRepository} methods to perform the required operations.
+ */
 @RestController
 @ExposesResourceFor(StarController.class)
 @RequestMapping("/stars")
@@ -15,10 +20,19 @@ public class StarController {
 
   private StarRepository starRepository;
 
+  /**
+   * Initializes this instance, injecting an instance of {@link StarRepository}
+   *
+   * @param starRepository repository used for operations on {@link Star} entity instances.
+   */
   public StarController(StarRepository starRepository) {
     this.starRepository = starRepository;
   }
 
+  /**
+   * Returns a randomly selected {@link Star} resource, presumably for use in star information
+   * retrieval-type applications.
+   */
   @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
   public Iterable<Star> get() {
     return starRepository.findFirst10ByOrderByAbsmag();
